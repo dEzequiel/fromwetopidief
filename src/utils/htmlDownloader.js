3113f13255d6bfaeda0e6ce472f3cc20c436d1c6
Ezequiel De La Rosa <linezekdlr@hotmail.com>
2024-07-08 11:07:30 +0200

    feat: util to download webpage html code

---
+++ src/utils/htmlDownloader.js
@@ -0,0 +1,14 @@
+class HTMLDownloader {
+    static async downloadHTML(url) {
+        try {
+            const response = await fetch(url);
+            const html = await response.text();
+            return html;
+        } catch (error) {
+            console.error('Error downloading HTML:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = HTMLDownloader;
